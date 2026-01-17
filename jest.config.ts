f781802d0a198f99b8e567d553e5a669b959de0e
davidalmeidasj <davidasj21@gmail.com>
2026-01-17 12:44:22 -0300

    feat - jest config

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/app/layout.tsx',
+  ],
+};
+
+export default createJestConfig(config);
